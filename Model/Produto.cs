f99a207958dc718c986d467df191b972e02ff0aa
Gabrielle Prado <gabriellesprado@gmail.com>
2021-11-08 18:57:09 -0300

    Melhorias para finalização do projeto.

---
+++ Model/Produto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace ProjetoLojaSimples.Entidade
+{
+    public class Produto
+    {
+        public int Codigo { get; set; }
+        public string Nome { get; set; }
+        public int Quantidade { get; set; }
+        public double Valor { get; set; }
+
+        public List<Produto> ListaProdutos = new List<Produto>();
+
+        public List<Produto> CarrinhoCompras = new List<Produto>();
+        public Produto(string nome, double valor, int codigo)
+        {
+            Nome = nome;
+            Valor = valor;
+            Codigo = codigo;
+        }
+        public Produto(string nome, double valor, int codigo, int quantidade)
+        {
+            Nome = nome;
+            Valor = valor;
+            Codigo = codigo;
+            Quantidade = quantidade;
+        }
+        public Produto()
+        {
+        }
+
+        public void AdicionaProduto(Produto produto)
+        {
+            ListaProdutos.Add(produto);
+        }
+
+    }
+}
---
+++ ProjetoLojaSimples/Service/ProdutoService.cs
@@ -0,0 +1,58 @@
+﻿using ProjetoLojaSimples.Entidade;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoLojaSimples.Negocio
+{
+    public class ProdutoService
+    {
+        Produto _produto = new Produto();
+        public double CalculaTotalCompra()
+        {
+            double total = 0.0;
+            foreach (Produto obj in _produto.CarrinhoCompras)
+            {
+                
+                total += obj.Quantidade * obj.Valor;
+            }
+            return total;
+        }
+
+        public void AdicionaProdutoCarrinho(Produto produto)
+        {
+            _produto.CarrinhoCompras.Add(produto);
+        }
+        public List<Produto> ListaCarrinhoCompras()
+        {
+            return _produto.CarrinhoCompras;
+        }
+
+            public void AdicionaProduto(Produto produto)
+        {
+            _produto.ListaProdutos.Add(produto);
+        }
+        public List<Produto> ListaProdutos()
+        {
+            AdicionaProduto(new Produto("Coca", 5.0, 1));
+            AdicionaProduto(new Produto("Café", 2.0, 2));
+            AdicionaProduto(new Produto("Maça", 2.50, 3));
+            AdicionaProduto(new Produto("Sal", 10.0, 4));
+            AdicionaProduto(new Produto("Arroz", 5.0, 5));
+            
+            return _produto.ListaProdutos;
+        }
+
+        public bool ValidaCompra(int quantidade)
+        {
+            if (quantidade > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
